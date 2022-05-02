ab2272027d6deceadc5dc470310170fc81e2fa88
Brianna Adewinmbi <brianna.adewinmbi@gmail.com>
2022-05-02 13:48:37 -0700

    Solve indexing error, add scene changing
    In concentration test, error was that the first image would not show up or would not change to next image when item was pressed. Scene changing from concentration test to reaction test

---
+++ cognitive-test/Assets/Scripts/Concentration.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Concentration : MonoBehaviour {
+
+    [SerializeField] private GameObject menuScene, testScene, completeScene; // Each "scene" is an empty game object used to group UI
+    [SerializeField] private Button[] itemButtons;
+    [SerializeField] private Image currentImage;
+    [SerializeField] private Text currentItemName;
+    private int currButtonIndex;
+    private Object[] images;
+    private string[] itemNames = { // Names of items that need to be found in images
+        "Four-Leaf Clover", "Cat", "Panda", "Heart", "Panda"
+    };
+
+    private void Start() {
+
+        menuScene.SetActive(true);
+        testScene.SetActive(false);
+        completeScene.SetActive(false);
+        images = Resources.LoadAll("ConcentrationImages", typeof(Sprite));
+
+        for (int i = 0; i < itemButtons.Length; i++) {
+            if (i != itemButtons.Length && i != itemButtons.Length - 1) {
+                int temp = i;
+                itemButtons[i].onClick.AddListener(() => { ItemFound(temp + 1); } ); // Call ItemFound() with correct index as parameter when each button is pressed
+            } else {
+                itemButtons[i].onClick.AddListener(FinishConcentrationTest);
+            }
+            itemButtons[i].gameObject.SetActive(false);
+        }
+
+        currButtonIndex = 0;
+        itemButtons[currButtonIndex].gameObject.SetActive(true);
+    }
+
+    private void FinishConcentrationTest() {
+        testScene.SetActive(false);
+        completeScene.SetActive(true);
+    }
+
+    private void ItemFound(int itemIndex) {
+        if (itemIndex == 0) { // Occurs during the first call to this method
+            itemButtons[itemIndex].gameObject.SetActive(true); // Show first image
+        } else {
+            itemButtons[itemIndex - 1].gameObject.SetActive(false);
+            itemButtons[itemIndex].gameObject.SetActive(true);
+        }
+
+        currentImage.sprite = (Sprite)images[itemIndex];
+        currentItemName.text = itemNames[itemIndex];
+    }
+
+    public void EnableTestScene() {
+        menuScene.SetActive(false);
+        testScene.SetActive(true);
+        completeScene.SetActive(false);
+        ItemFound(0); // Start
+    }
+
+    public void MoveToReactionTest() {
+        SceneManager.LoadScene("ReactionTest");
+    }
+
+}
